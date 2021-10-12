101ee03ed68c18511224ae5bf7bba1b970f3ec2b
sksurendrakmr <sksurendrakmr@gmail.com>
2021-10-12 15:08:10 +0530

    Initialize createNativeStackNavigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React, {ReactNode} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 type ScreenProps = {
   children: ReactNode;
 };
@@ -20,6 +22,16 @@ const TweetDetails = () => (
     <Text>Tweet Details</Text>
   </Screen>
 );
+
+/**
+ * this function return an Object.
+ * The return object has couple of preperties -> Navigator and Screen
+ * Both these properties are react component.
+ *
+ * That's why we named the object using pascal naming convention as we will
+ * use this as react component.
+ */
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return <View></View>;
